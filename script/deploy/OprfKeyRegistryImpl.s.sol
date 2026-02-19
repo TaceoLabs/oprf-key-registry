d67b582b5e80632f576260c3d58d1aee9117b9a8
0xThemis <118167989+0xThemis@users.noreply.github.com>
2026-02-19 08:34:59 +0100

    chore: removed dead code in sol script

--- script/deploy/OprfKeyRegistryImpl.s.sol
+++ script/deploy/OprfKeyRegistryImpl.s.sol
@@ -3,11 +3,9 @@ pragma solidity ^0.8.20;
 
 import {Script, console} from "forge-std/Script.sol";
 import {OprfKeyRegistry} from "../../src/OprfKeyRegistry.sol";
-import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract DeployOprfKeyRegistryImplScript is Script {
     OprfKeyRegistry public oprfKeyRegistry;
-    ERC1967Proxy public proxy;
 
     function setUp() public {}
 
